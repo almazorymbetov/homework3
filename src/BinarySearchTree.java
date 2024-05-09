6c874384148fed819b1c462fc16b16049bcb4c20
Almaz Orymbetov <230585@astanait.edu.kz>
2024-05-09 12:33:47 +0500

    Binary

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,6 @@
-public class BinarySearchTree <K extends Comparable<K>, V>{
+import java.util.LinkedList;
+import java.util.Queue;
+public class BinarySearchTree <K extends Comparable<K>,V>{
     private Node root;
     private class Node{
         private K key;
@@ -10,15 +12,27 @@ public class BinarySearchTree <K extends Comparable<K>, V>{
         }
     }
     public void put(K key,V val){
-
+        root=put(root,key,val);
     }
     public V get(K key){
-
+        Node x=root;
+        while(x!=null){
+            int comp=key.compareTo(x.key);
+            if(comp<0){
+                x=x.left;
+            }else if(comp>0){
+                x=x.right;
+            }else{
+                return x.val;
+            }
+        }
+        return null;
     }
     public void delete(K key){
-
+        root =delete(root,key);
     }
     public Iterable<K> iterator(){
-
+        Queue<K> keys=new LinkedList<>();
+        return keys;
     }
 }
--- src/HashNode.java
+++ src/HashNode.java
@@ -1,10 +1,11 @@
-public class HashNode {
+public class HashNode<K,V>{
     private K key;
     private V value;
     private HashNode<K,V> next;
     public HashNode(K key,V value){
         this.key=key;
         this.value=value;
+        this.next=null;
     }
     @Override
     public String toStr(){
