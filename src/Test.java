89a97776ed21fa362cd188c66399113dfdf04147
Almaz Orymbetov <230585@astanait.edu.kz>
2024-05-12 15:06:06 +0500

    Test

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+public class Test {
+    public int value;
+    public Test(int value){
+        this.value=value;
+    }
+    public static void main(String[] args){
+        /*
+        testing K
+
+        Random rand = new Random();
+        MyHashTable<Test,String> table =new MyHashTable<>(99999);
+        for(int i=0;i<99999;i++){
+            int randv=rand.nextInt(99999);
+            Test key =new Test(randv);
+            table.put(key," " + randv);
+        }
+        for(int i=0;i<table.table.length;i++){
+            System.out.println(i+" "+table.table[i].size());
+        }
+         */
+
+        BinarySearchTree<Integer,String> tree = new BinarySearchTree<>();
+        tree.put(45,"half of 90");
+        tree.put(3,"three");
+        tree.put(999,"999");
+        for (BinarySearchTree<Integer,String>.iteration iter : tree.iterator()) {
+            System.out.println("key is"+iter.getKey()+" and value is "+iter.getValue());
+        }
+    }
+}
